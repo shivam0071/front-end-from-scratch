e1788936680e38bcb4daef4de0da459c0683f178
shivam0071 <shivamkoundal01@gmail.com>
2025-01-25 17:48:26 +0530

    Just Practice

---
+++ 50Projects/2_Progress_Steps/2_1_Practice_Steps/scripts.js
@@ -0,0 +1,53 @@
+const progress = document.getElementById('progress');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+const circles = document.querySelectorAll('.circle');
+
+let progressCounter = 1;
+
+next.addEventListener('click', () => {
+  progressCounter++;
+  if(progressCounter >= circles.length){
+    progressCounter = circles.length;
+  }
+  else if(progressCounter <= 1) {
+    progressCounter = 1;
+  }
+  update()
+});
+
+prev.addEventListener('click', () => {
+  progressCounter--;
+  if(progressCounter >= circles.length){
+    progressCounter = circles.length;
+  }
+  else if(progressCounter <= 1) {
+    progressCounter = 1;
+  }
+  update()
+});
+
+function update(){
+  circles.forEach((circle, idx) => {
+    if(idx < progressCounter){
+      circle.classList.add('active');
+    }
+    else{
+      circle.classList.remove('active');
+    }
+  });
+
+  const active = document.querySelectorAll('.active');
+  progress.style.width = ((active.length - 1) / (circles.length - 1)) * 100 + '%'
+
+  if(progressCounter == 1){
+    prev.disabled = true;
+  }
+  else if(progressCounter == circles.length){
+    next.disabled = true;
+  }
+  else{
+    prev.disabled = false;
+    next.disabled = false;
+  }
+}

4c39faad350356e6922550a5a527a2070e9e6885
shivam0071 <shivamkoundal01@gmail.com>
2025-02-06 18:05:38 +0530

    array and loops

--- JavaScript/scripts/11-todo-list.js
+++ JavaScript/scripts/11-todo-list.js
@@ -1,15 +1,52 @@
-const todoList = [];
+const todoList = [{
+  name: 'Make dinner',
+  dueDate: '2025-02-15'
+},
+{
+  name: 'Make FB',
+  dueDate: '2025-02-15'
+}];
+
 
 function addTodo(){
   const inputElement = document.querySelector('.js-name-input');
+  const dateElement = document.querySelector('.js-date-input');
+ 
+
   const name = inputElement.value;
+  const dueDate = dateElement.value;
+
   if (name !== ''){
-    todoList.push(name);
+    todoList.push({
+      name, dueDate
+    });
   }
   else{
-    console.log('Nothings New, Nothings New, Nothings New, Nothings New')
+    console.log('Nothings New')
+  }
+  inputElement.value = '';
+  renderToDoList();
+}
+
+
+
+function renderToDoList(){
+  let todoListHtml = '';
+
+  for(let i=0; i<todoList.length; i++){
+    const {name, dueDate} = todoList[i];
+    todoListHtml += `
+    <div>${name}</div> 
+    <div>${dueDate}</div>
+    <button onclick="
+    todoList.splice(${i}, 1);
+    renderToDoList();
+    ">Delete</button>
+    `;
   }
 
-  inputElement.value = '';
-  console.log(todoList);
+  let jsResult = document.querySelector('.js-result');
+  jsResult.innerHTML = todoListHtml;
 }
+
+renderToDoList();
